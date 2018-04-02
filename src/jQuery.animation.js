6c229328d53c628d6447c51a57fe7a06786afb3f
XiangJun-Chen <m18189532448_1@163.com>
2018-04-02 11:38:28 +0800

    4/2

---
+++ src/jQuery.animation.js
@@ -0,0 +1,61 @@
+(function($) {
+	
+	var defaults = {
+		
+		//动画名称
+		name: '',
+		
+		//动画持续时间
+		time: '1.5s'
+		
+	}
+	
+	$.fn.extend({
+		animation: function(opt) {
+			var options = $.extend(defaults , opt),
+				str = 'bounce,flash,pulse,rubberBand,shake,swing,tada,wobble,flip,hinge',
+				str1 = 'bounceIn,bounceInDown,bounceInLeft,bounceInRight,bounceInUp,fadeIn,fadeInDown,fadeInDownBig,fadeInLeft,fadeInLeftBig,fadeInRight,fadeInRightBig,fadeInUp,fadeInUpBig,flipInX,flipInY,lightSpeedIn,rotateIn,rotateInDownLeft,rotateInDownRight,rotateInUpLeft,rotateInUpRight,slideInDown,slideInLeft,slideInRight,rollIn';
+			
+			return this.each(function() {
+				$(this).addClass('animated').css({
+					"animation-duration": options.time
+				});
+				if(str.indexOf(options.name) > -1) {
+					$(this).hover(function() {
+						$(this).addClass(options.name);
+					},function() {
+						$(this).removeClass(options.name);
+					});
+				}else if(str1.indexOf(options.name) > -1) {
+					var $this = $(this);
+						$window = $(window),
+						$document = $(document),
+						windowH = $window.height(),
+						bottomH = $document.height() - windowH,
+						offsetH = $this.offset().top,
+						viewH = offsetH - windowH * .7;
+						
+					
+					viewH = (viewH > bottomH) ? bottomH : viewH;
+					$this.css("visibility","hidden");
+					if(windowH >= offsetH) {
+						$this.css({
+							"visibility": "visible"
+						});
+						$this.addClass(options.name);
+					}else {
+						$window.scroll(function() {
+							if($window.scrollTop() >= viewH) {
+								$this.css({
+									"visibility": "visible"
+								});
+								$this.addClass(options.name);
+							}
+						});
+					}
+				}
+			});
+		}
+	});
+	
+})(jQuery);
