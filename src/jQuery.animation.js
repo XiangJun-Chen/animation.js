fc2614c35133bfd4c37986d930bf0941e84431da
XiangJun-Chen <m18189532448_1@163.com>
2018-04-04 11:54:18 +0800

    4/4

--- src/jQuery.animation.js
+++ src/jQuery.animation.js
@@ -1,41 +1,41 @@
 (function($) {
 	
-	var defaults = {
-		
-		//动画名称
-		name: '',
-		
-		//动画持续时间
-		time: '1.5s'
-		
-	}
-	
 	$.fn.extend({
 		animation: function(opt) {
+			
+			var defaults = {
+				
+				//动画名称
+				name: '',
+				
+				//动画持续时间
+				time: '1.5s'
+				
+			}
+			
 			var options = $.extend(defaults , opt),
 				str = 'bounce,flash,pulse,rubberBand,shake,swing,tada,wobble,flip,hinge',
 				str1 = 'bounceIn,bounceInDown,bounceInLeft,bounceInRight,bounceInUp,fadeIn,fadeInDown,fadeInDownBig,fadeInLeft,fadeInLeftBig,fadeInRight,fadeInRightBig,fadeInUp,fadeInUpBig,flipInX,flipInY,lightSpeedIn,rotateIn,rotateInDownLeft,rotateInDownRight,rotateInUpLeft,rotateInUpRight,slideInDown,slideInLeft,slideInRight,rollIn';
 			
 			return this.each(function() {
-				$(this).addClass('animated').css({
+				var $this = $(this);
+				$this.addClass('animated').css({
 					"animation-duration": options.time
 				});
 				if(str.indexOf(options.name) > -1) {
-					$(this).hover(function() {
-						$(this).addClass(options.name);
+					$this.hover(function() {
+						$this.addClass(options.name);
 					},function() {
-						$(this).removeClass(options.name);
-					});
+						$this.removeClass(options.name);
+					});				
 				}else if(str1.indexOf(options.name) > -1) {
-					var $this = $(this);
-						$window = $(window),
+					var	$window = $(window),
 						$document = $(document),
 						windowH = $window.height(),
 						bottomH = $document.height() - windowH,
 						offsetH = $this.offset().top,
 						viewH = offsetH - windowH * .7;
 						
-					
 					viewH = (viewH > bottomH) ? bottomH : viewH;
 					$this.css("visibility","hidden");
 					if(windowH >= offsetH) {
@@ -51,7 +51,7 @@
 								});
 								$this.addClass(options.name);
 							}
-						});
+						})						
 					}
 				}
 			});
